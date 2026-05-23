b7aa2f78633264fb44bd5ab9d4413fba2715ecdd
dgucik <danielgucik7@gmail.com>
2026-05-23 14:13:19 +0200

    Move global templates to pages

--- pages/urls.py
+++ pages/urls.py
@@ -3,12 +3,9 @@ from django.contrib.auth.views import LogoutView
 
 from pages.views import HomePageView, AboutPageView, LoginPageView
 
-def get_urls():
-    return [
+urlpatterns = [
         path("login/", LoginPageView.as_view(), name="login"),
         path("logout/", LogoutView.as_view(), name="logout"),
         path("", HomePageView.as_view(), name="home"),
         path("about/", AboutPageView.as_view(), name="about"),
     ]
-
-urlpatterns = get_urls()
